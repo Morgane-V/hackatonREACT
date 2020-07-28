8280c133836f7c3500322ffac1bfe1a0c67b03fe
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-07-28 12:20:26 +0200

    fix

--- api_symfony/src/Entity/Post.php
+++ api_symfony/src/Entity/Post.php
@@ -12,144 +12,144 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Post
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+   * @ORM\Id()
+   * @ORM\GeneratedValue()
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $title;
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $title;
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $content;
+  /**
+   * @ORM\Column(type="text")
+   */
+  private $content;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
+  /**
+   * @ORM\Column(type="datetime")
+   */
+  private $createdAt;
 
-    /**
-     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="post")
-     */
-    private $comments;
+  /**
+   * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="post")
+   */
+  private $comments;
 
-    /**
-     * @ORM\OneToMany(targetEntity=fish::class, mappedBy="post")
-     */
-    private $fish;
+  /**
+   * @ORM\OneToMany(targetEntity=Fish::class, mappedBy="post")
+   */
+  private $Fish;
 
-    public function __construct()
-    {
-        $this->comments = new ArrayCollection();
-        $this->fish = new ArrayCollection();
+  public function __construct()
+  {
+    $this->comments = new ArrayCollection();
+    $this->Fish = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getTitle(): ?string
+  {
+    return $this->title;
+  }
+
+  public function setTitle(string $title): self
+  {
+    $this->title = $title;
+
+    return $this;
+  }
+
+  public function getContent(): ?string
+  {
+    return $this->content;
+  }
+
+  public function setContent(string $content): self
+  {
+    $this->content = $content;
+
+    return $this;
+  }
+
+  public function getCreatedAt(): ?\DateTimeInterface
+  {
+    return $this->createdAt;
+  }
+
+  public function setCreatedAt(\DateTimeInterface $createdAt): self
+  {
+    $this->createdAt = $createdAt;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection|Comment[]
+   */
+  public function getComments(): Collection
+  {
+    return $this->comments;
+  }
+
+  public function addComment(Comment $comment): self
+  {
+    if (!$this->comments->contains($comment)) {
+      $this->comments[] = $comment;
+      $comment->setPost($this);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeComment(Comment $comment): self
+  {
+    if ($this->comments->contains($comment)) {
+      $this->comments->removeElement($comment);
+      // set the owning side to null (unless already changed)
+      if ($comment->getPost() === $this) {
+        $comment->setPost(null);
+      }
     }
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
+    return $this;
+  }
+
+  /**
+   * @return Collection|Fish[]
+   */
+  public function getFish(): Collection
+  {
+    return $this->Fish;
+  }
+
+  public function addFish(Fish $Fish): self
+  {
+    if (!$this->Fish->contains($Fish)) {
+      $this->Fish[] = $Fish;
+      $Fish->setPost($this);
     }
 
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
+    return $this;
+  }
 
-        return $this;
+  public function removeFish(Fish $Fish): self
+  {
+    if ($this->Fish->contains($Fish)) {
+      $this->Fish->removeElement($Fish);
+      // set the owning side to null (unless already changed)
+      if ($Fish->getPost() === $this) {
+        $Fish->setPost(null);
+      }
     }
 
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
-
-    public function setContent(string $content): self
-    {
-        $this->content = $content;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Comment[]
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): self
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments[] = $comment;
-            $comment->setPost($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): self
-    {
-        if ($this->comments->contains($comment)) {
-            $this->comments->removeElement($comment);
-            // set the owning side to null (unless already changed)
-            if ($comment->getPost() === $this) {
-                $comment->setPost(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|fish[]
-     */
-    public function getFish(): Collection
-    {
-        return $this->fish;
-    }
-
-    public function addFish(fish $fish): self
-    {
-        if (!$this->fish->contains($fish)) {
-            $this->fish[] = $fish;
-            $fish->setPost($this);
-        }
-
-        return $this;
-    }
-
-    public function removeFish(fish $fish): self
-    {
-        if ($this->fish->contains($fish)) {
-            $this->fish->removeElement($fish);
-            // set the owning side to null (unless already changed)
-            if ($fish->getPost() === $this) {
-                $fish->setPost(null);
-            }
-        }
-
-        return $this;
-    }
+    return $this;
+  }
 }
